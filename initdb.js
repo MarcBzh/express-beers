aadff59a2a431228a80f550d48e35ea77f86fd5f
MarcRo <marcrousseau77@yahoo.fr>
2020-03-09 16:50:01 +0100

    initdb

--- initdb.js
+++ initdb.js
@@ -1,4 +1,5 @@
 var MongoClient = require('mongodb').MongoClient;
+
 var url = process.env.MONGODB_ADDON_URI;
 var dbName = process.env.MONGODB_ADDON_DB;
 var beers = [
@@ -14,16 +15,34 @@ var beers = [
     'TrappistesRochefort8',
     'TrappistesRochefort10'
 ]
-/*async function initDb(){
-    try{
+
+
+async function initDb() {
+    try {
         client = await MongoClient.connect(url);
         const db = client.db(dbName);
-        let dropped = await db.beers.drop();
-        beers.forEach(async beerName) => { 
-            let beer = require(' ./step-05/beers/${beerName}.json');
-            let inserted = await db.collection.('beers').insertOne(beer);
-            console.log('Beer ${beerName} inserted');
-        };
+        console.log('Deleting beers');
+        try {
+            let dropped = await db.collection('beers').drop();
+            console.log('DB dropped');
+        } catch(err) {
+            console.log('Collection not found', err);
+        }
+        console.log('Inserting beers');
+        for (let i in beers) {
+            let beerName = beers[i];
+            console.log(`Inserting ${beerName}`);
+            let beer = require(`./step-05/beers/${beerName}.json`);
+            console.log(`Inserting ${beerName}`);
+            let inserted = await db.collection('beers').insertOne(beer);
+            console.log(`Beer ${beerName} inserted`);
+        }
         return process.exit(0);
     }
-}*/
+    catch(err) {
+        console.log('InitDb error', err);
+        return process.exit(0);
+    }
+} 
+
+initDb();
