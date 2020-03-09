7ca3c3e266b47c919b5e075861aab18fb941fa99
MarcRo <marcrousseau77@yahoo.fr>
2020-03-09 15:37:32 +0100

    correction

--- step-05/index.js
+++ step-05/index.js
@@ -45,7 +45,7 @@ var dbName = process.env.MONGODB_ADDON_DB;
 
 
 
-var server = app.listen(process.env.PORT, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log('Listening at http://%s:%s', host, port);
