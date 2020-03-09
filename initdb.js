97e2f9afdffe828214256395d0121ef574faa2c7
MarcRo <marcrousseau77@yahoo.fr>
2020-03-09 15:59:06 +0100

    initdb

--- initdb.js
+++ initdb.js
@@ -19,7 +19,7 @@ async function initDb(){
         client = await MongoClient.connect(url);
         const db = client.db(dbName);
         let dropped = await db.beers.drop();
-        beers.forEach(async beerName) => {
+        beers.forEach(async beerName) => { 
         }
     }
 }
